ff9c4528e8fab121bc627f4f256fd8a2adebbe08
Jingmeng Li <spica_zhiji@163.com>
2019-09-24 21:59:17 +0800

    Deleted SystemCode

--- SystemCode/Backend/src/main/java/trippingo/repository/ItineraryRepository.java
+++
@@ -1,9 +0,0 @@
-package trippingo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import trippingo.model.Itinerary;
-
-public interface ItineraryRepository extends CrudRepository<Itinerary, Long>{
-
-}
